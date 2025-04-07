e2c9bc9e777c47927a76c381d93781a7f69ac096
Najma Sultani <121963482+najmasultani@users.noreply.github.com>
2025-04-07 00:06:10 -0400

    Create generate_scaler.py

---
+++ SmartStudy/smartstudy/generate_scaler.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import joblib
+from pathlib import Path
+
+from config import PROCESSED_DATA_DIR, MODELS_DIR
+
+data = pd.read_csv(PROCESSED_DATA_DIR / "processed_data.csv")
+
+features = data.drop(columns=["GPA"])
+
+scaler = StandardScaler()
+scaler.fit(features)
+
+scaler_path = MODELS_DIR / "scaler.pkl"
+MODELS_DIR.mkdir(parents=True, exist_ok=True)
+joblib.dump(scaler, scaler_path)
+
+print(f"The Scaler is saved to: {scaler_path}")
