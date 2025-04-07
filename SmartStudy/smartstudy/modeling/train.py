8d46e514bce6e194307b00214f265b5530140411
Elorie Bernard-Lacroix <eloriel0310@gmail.com>
2025-04-06 21:22:22 -0400

    trained the tabpfn model officially

--- SmartStudy/smartstudy/modeling/train.py
+++ SmartStudy/smartstudy/modeling/train.py
@@ -3,6 +3,10 @@ from pathlib import Path
 import typer
 from loguru import logger
 from tqdm import tqdm
+import pandas as pd
+from tabpfn import TabPFNRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import joblib
 
 from smartstudy.config import MODELS_DIR, PROCESSED_DATA_DIR
 
@@ -12,17 +16,44 @@ app = typer.Typer()
 @app.command()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    labels_path: Path = PROCESSED_DATA_DIR / "labels.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
+    features_path: Path = PROCESSED_DATA_DIR / "train_features.csv",
+    labels_path: Path = PROCESSED_DATA_DIR / "train_labels.csv",
+    model_path: Path = MODELS_DIR / "tabpfn.pkl",
     # -----------------------------------------
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
+    # ---- TabPFN ----
     logger.info("Training some model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
+    logger.info(f"Features path: {features_path}")
+    logger.info(f"Labels path: {labels_path}")
+    logger.info(f"Model path: {model_path}")
+    logger.info("Loading data...")
+
+    # Loading data
+    features = pd.read_csv(features_path)
+    labels = pd.read_csv(labels_path)
+    logger.info("Data loaded successfully.")
+
+    # Training model
+    logger.info("Training model...")
+    model = TabPFNRegressor(random_state=42)
+    model.fit(features, labels)
+    logger.info("Model trained successfully.")
+    
+    # Saving model
+    logger.info("Saving model...")
+    joblib.dump(model, model_path)
+    logger.info("Model saved successfully.")
+
     logger.success("Modeling training complete.")
+
+    # -----------------------------------------
+    # ------ Bayesian Opt ---------------------
+    
+    
+
+    # -----------------------------------------
+    # ------ Weighted KNN ---------------------
+
     # -----------------------------------------
 
 
